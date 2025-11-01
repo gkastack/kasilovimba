b587c5fabb61902547d308404a16e679b4c022f8
Comerce Group <grcomerce@IUS.LOCAL>
2025-11-01 13:06:41 +0300

    created create and destroy functions

--- 1.cpp
+++ 1.cpp
@@ -1,3 +1,38 @@
 #include <iostream>
 
-int main(){}
+int** create(size_t rows, size_t cols)
+{
+    int** mtx = new int*[rows];
+    size_t count = 0;
+    try
+    {
+        for (; count < rows; count++)
+            mtx[count] = new int[cols];
+    }
+    catch (const std::bad_alloc& e)
+    {
+        destroy(mtx, count);
+    }
+}
+
+void destroy(int** mtx, size_t count)
+{
+    for (size_t i = 0; i < count; i++)
+        delete[] mtx[i];
+    delete[] mtx;
+}
+
+int main()
+{
+    int** matrix = nullptr;
+    try
+    {
+        matrix = create(5, 5);
+    }
+    catch (const std::bad_alloc & e)
+    {
+        std::cerr << e.what();
+        return 1;
+    }
+    destroy(matrix, 5);
+}
