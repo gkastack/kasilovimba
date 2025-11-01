5be96ce4244e34b4a411fa0632b2508f534a91f3
Comerce Group <grcomerce@IUS.LOCAL>
2025-11-01 13:34:32 +0300

    created input function

--- 1.cpp
+++ 1.cpp
@@ -6,33 +6,46 @@ int** create(size_t rows, size_t cols)
     size_t count = 0;
     try
     {
-        for (; count < rows; count++)
+        for (; count < rows; ++count)
             mtx[count] = new int[cols];
     }
     catch (const std::bad_alloc& e)
     {
         destroy(mtx, count);
+        throw;
     }
 }
 
 void destroy(int** mtx, size_t count)
 {
-    for (size_t i = 0; i < count; i++)
+    for (size_t i = 0; i < count; ++i)
         delete[] mtx[i];
     delete[] mtx;
 }
 
+void input(int** mtx, int init, size_t rows, size_t cols)
+{
+    for(int i = 0; i < rows; ++i)
+    {
+        for(int j = 0; j < cols; ++j)
+            mtx[i][j] = init;
+    }
+}
+
 int main()
 {
+    size_t rows, cols;
+    std::cin << rows << cols;
     int** matrix = nullptr;
     try
     {
-        matrix = create(5, 5);
+        matrix = create(rows, cols);
     }
     catch (const std::bad_alloc & e)
     {
         std::cerr << e.what();
         return 1;
     }
-    destroy(matrix, 5);
+    input(mtx, 2, rows, cols);
+    destroy(matrix, rows);
 }
